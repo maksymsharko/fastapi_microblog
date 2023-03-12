ccda6f7525203af27363622d7daa48a16d8914f6
maksymsharko <maks.sharko02@gmail.com>
2023-03-12 17:51:56 +0200

    First commit; Added two models: user and post

---
+++ core/db.py
@@ -0,0 +1,9 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+SQLALCHEMY_DATABASE_URL = "postgresql://myuser:mypassfastapi@localhost/fastapi_channel"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+Sessionlocal = sessionmaker(autocommit=False,autoflush=False, bind=engine)
+Base = declarative_base()
---
+++ microblog/models.py
@@ -0,0 +1,14 @@
+from core.db import Base
+from sqlalchemy import Column, String, Integer, Text, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
+
+
+class Post(Base):
+    __tablename__ = "microblog_posts"
+    id = Column(Integer, primary_key=True, index=True, unique=True)
+    title = Column(String)
+    text = Column(Text)
+    date = Column(DateTime)
+    user = Column(Integer, ForeignKey("user.id"))
+    user_id = relationship("User")
+
